df46efebc64adc6b9858512aad2ee69ee985105c
Nishant Kumar <niisshhaannt@gmail.comm>
2025-08-06 12:27:59 +0530

    message

--- main.tf
+++ main.tf
@@ -34,3 +34,7 @@ resource "azurerm_resource_group" "resource3" {
   name     = "pipeline-resources3"
   location = "centralindia"
 }
+resource "azurerm_resource_group" "resource4" {
+  name     = "pipeline-resources4"
+  location = "centralindia"
+}
